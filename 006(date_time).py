db1887c03acac8c5b0400103fc50f82cb56e9edb
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-08-29 11:43:19 -0300

    time

--- 006(date_time).py
+++ 006(date_time).py
@@ -3,5 +3,5 @@ import datetime
 Dia_hoje = datetime.date.today()
 print(Dia_hoje)
 
-Hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+Hora_atual = datetime.datetime.now()
 print(Hora_atual)
